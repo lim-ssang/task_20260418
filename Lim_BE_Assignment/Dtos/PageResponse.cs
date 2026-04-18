e428efe64626c426423caa33e181e61f1b973c02
m_ssang <m_ssang@outlook.com>
2026-04-18 16:57:04 +0900

    초기 커밋.

---
+++ Lim_BE_Assignment/Dtos/PageResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Lim_BE_Assignment.Dtos
+{
+    public class PageResponse<T>
+    {
+        public int Total { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int MaxPage { get; set; }
+        public List<T> Items { get; set; } = new List<T>();
+    }
+}
---
+++ Lim_BE_Assignment/Entites/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Lim_BE_Assignment.Entites
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string TelNo { get; set; }
+        public DateTimeOffset JoinedAt { get; set; }
+    }
+}
